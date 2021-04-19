ee3ade82366d5ffd3ed756e31f9eafde0231c1cc
Connie Chen <conne.chen1101@users.noreply.github.com>
2021-04-19 17:44:47 -0400

    Working on Current Order

--- app/src/main/java/com/example/rucafe/CurrentOrderActivity.java
+++ app/src/main/java/com/example/rucafe/CurrentOrderActivity.java
@@ -7,19 +7,28 @@ import android.os.Bundle;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class CurrentOrderActivity extends AppCompatActivity {
     ListView ordersListview;
     Button removeOrderButton, placeOrderButton;
     TextView subtotalTextView, taxTextView, totalTextView;
 
-
+    ArrayList<MenuItem> itemsToDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_order);
-        ordersListview = (ListView) findViewById(R.id.ordersListView);
 
+        //Initializing ListView with Orders
+        ordersListview = (ListView) findViewById(R.id.ordersListView);
+        for(int i = 0; i < MainActivity.currOrder.items.size(); i++){
+            itemsToDisplay.add(MainActivity.currOrder.items.get(i));
+        }
+
+        ArrayAdapter<MenuItem> adapter = new ArrayAdapter<MenuItem>(this, android.R.layout.simple_list_item_1, itemsToDisplay);
+        ordersListview.setAdapter(adapter);
         removeOrderButton = (Button) findViewById(R.id.removeOrderButton);
         placeOrderButton = (Button) findViewById(R.id.placeOrderButton);
         subtotalTextView = (TextView) findViewById(R.id.subtotalDisplayTextView);
