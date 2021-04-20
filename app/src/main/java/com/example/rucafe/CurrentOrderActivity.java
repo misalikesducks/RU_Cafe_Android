21a8ca789e245f0dd38fbe2a5d4723a4690da6b0
Connie Chen <conne.chen1101@users.noreply.github.com>
2021-04-19 23:07:05 -0400

    dsklfhklea

--- app/src/main/java/com/example/rucafe/CurrentOrderActivity.java
+++ app/src/main/java/com/example/rucafe/CurrentOrderActivity.java
@@ -9,34 +9,42 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class CurrentOrderActivity extends AppCompatActivity {
+public class CurrentOrderActivity extends AppCompatActivity{
     ListView ordersListview;
     Button removeOrderButton, placeOrderButton;
     TextView subtotalTextView, taxTextView, totalTextView;
 
-    protected ArrayList<MenuItem> itemsToDisplay;
-    protected MenuItemAdapter menuAdaptor;
+    protected ArrayList<MenuItem> itemsToDisplay = new ArrayList<>();
+    protected ArrayAdapter<MenuItem> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_order);
 
-        //Initializing ListView with Orders
-        ordersListview = (ListView) findViewById(R.id.ordersListView);
-        for(int i = 0; i < MainActivity.currOrder.items.size(); i++){
-            itemsToDisplay.add(MainActivity.currOrder.items.get(i));
-        }
 
-        menuAdaptor = new MenuItemAdapter(this, itemsToDisplay);
-        ordersListview.setAdapter(menuAdaptor);
-
-
+//        ordersListview.setOnClickListener(this);
+        populateListView();
         removeOrderButton = (Button) findViewById(R.id.removeOrderButton);
         placeOrderButton = (Button) findViewById(R.id.placeOrderButton);
         subtotalTextView = (TextView) findViewById(R.id.subtotalDisplayTextView);
         taxTextView = (TextView) findViewById(R.id.salesTaxDisplayTextView);
         totalTextView = (TextView) findViewById(R.id.totalDisplayTextView);
     }
+
+    public void populateListView(){
+        for(int i = 0; i < MainActivity.currOrder.items.size(); i++){
+            itemsToDisplay.add(MainActivity.currOrder.items.get(i));
+        }
+        adapter = new ArrayAdapter
+                (this, R.layout.currorder_listitem,R.id.displayTextView,itemsToDisplay);
+        ordersListview = (ListView) findViewById(R.id.orderDisplayListView);
+        ordersListview.setAdapter(adapter);
+    }
+
+
+
+
+    // public void populateListView()
     
     /*
         CurrentOrder
